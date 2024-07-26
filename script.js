4b5e7b6f233aa6954545578cb0d8418f56d2dc01
Saurabh Sharma <132290433+saurabh-945798@users.noreply.github.com>
2024-07-26 17:19:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-const galleryImages = document.querySelectorAll('.galleryImages');
+
+
+const galleryImages = document.querySelectorAll('.gallery-image');
 const modal = document.getElementById('modal');
-const modalImage = document.getElementById("modal-images")
-const closebtm = document.querySelector('.clone')
+const modalImage = document.getElementById('modal-image');
+const closeBtn = document.querySelector('.close');
 
-galleryImages.forEach(Image =>{
-    images.addenetlistener('click', () =>{
-        modal.style.display = 'block'
-        modalImage.src =images.src
-        modalImage.style.width = '500px'
-        modalImage.style.height = '500px'
-
-
-
+galleryImages.forEach(image => {
+    image.addEventListener('click', () => {
+        modal.style.display = 'block';
+        modalImage.src = image.src;
+        modalImage.style.width = '500px';
+        modalImage.style.height = '500px';
     });
+});
 
-modal.addEventListener('click', (e) =>{
-    if (e.target === modal){
-        modal.style.display = 'none'
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = 'none';
     }
-})
+});
 
-close.btn.addEventListener('click'),()=>{
-    modal.style.display = 'none'
-}
-})
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
